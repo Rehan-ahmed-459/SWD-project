81c0d1aee6dd35b71cf260a9a658a6717765ae0c
Rehan ahmed <72250935+Rehan-ahmed-459@users.noreply.github.com>
2023-12-12 21:31:53 +0530

    Update connect.php

--- computer-science.atwebpages.com/Attendance/admin/connect.php
+++ computer-science.atwebpages.com/Attendance/admin/connect.php
@@ -1,7 +1,7 @@
 <?php
 //establishing connection with database.
 
-$con=mysqli_connect('localhost','root','','attmgsystem');
+$con=mysqli_connect('localhost','root','','attmanagement');
 if (!$con){
     echo '<script>alert("Connection Error")</script>';
 }
